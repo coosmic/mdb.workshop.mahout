fb5881fe7b525f54820327adfff72bf98c0a0f9f
RRRs <ramon.janssen@stud.hn.de>
2019-06-25 18:19:16 +0200

    add ex project

---
+++ matrix_multiplikation/src/main/scala/App.scala
@@ -0,0 +1,15 @@
+import org.apache.mahout.math._
+import org.apache.mahout.math.scalabindings._
+import collection.JavaConversions._
+import RLikeOps._
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent._
+import scala.concurrent.duration._
+object App {
+
+  def main(args: Array[String]) {
+
+    println("Hello World!")
+  }
+
+}
